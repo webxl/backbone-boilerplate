166ed5163469c9048a2944279d60ce6c073bd15d
webxl <mm@webxl.net>
2012-01-11 22:46:52 -0800

    Added 'reload' task, along with node server support, Issue #10

--- build/config.js
+++ build/config.js
@@ -40,7 +40,7 @@ config.init({
 
   watch: {
     files: ["assets/**/*", "app/**/*"],
-    tasks: "lint:files concat jst",
+    tasks: "lint:files concat jst reload",
 
     min: {
       files: ["assets/**/*", "app/**/*"],
@@ -50,7 +50,11 @@ config.init({
 
   clean: {
     folder: "dist/"
-  }
+  },
+
+	reload: {
+		dummy: "grunt requires this why?"
+	}
 
 });
 
